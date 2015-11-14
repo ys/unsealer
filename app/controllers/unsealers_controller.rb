8f431907ff8fb5df55468078169370704286626b
Yannick <yannick.schutz@gmail.com>
2015-11-14 23:40:51 +0100

    Use a form Luke

--- app/controllers/unsealers_controller.rb
+++ app/controllers/unsealers_controller.rb
@@ -5,17 +5,17 @@ class UnsealersController < ApplicationController
   end
 
   USER_AGENT = "VaultRuby/0.0.1 (+github.com/hashicorp/vault-ruby)".freeze
+
   # The default headers that are sent with every request.
   DEFAULT_HEADERS = {
     "Content-Type" => "application/json",
     "Accept"       => "application/json",
     "User-Agent"   => USER_AGENT,
   }
+
   def create
     shard = ENV["VAULT_SHARD"]
-    # @status = Vault.sys.unseal()
-    require "pp"
-    pp Excon.put("#{ENV["VAULT_ADDR"]}/v1/sys/unseal", body: {key: shard}.to_json, headers: DEFAULT_HEADERS)
+    Excon.put("#{ENV["VAULT_ADDR"]}/v1/sys/unseal", body: {key: shard}.to_json, headers: DEFAULT_HEADERS)
   rescue StandardError => e
     flash[:error] = e.message
   ensure
